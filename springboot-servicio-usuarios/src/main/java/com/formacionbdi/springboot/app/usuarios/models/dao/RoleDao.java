b45dcc4840ac99d3a47100e376dd3748f31c4c09
Juan Fidel <yashirorko@gmail.com>
2024-07-30 13:26:55 -0600

    zuul, productos y usuarios

---
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/RoleDao.java
@@ -0,0 +1,11 @@
+package com.formacionbdi.springboot.app.usuarios.models.dao;
+
+//import org.springframework.data.repository.PagingAndSortingRepository;
+//import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+//
+//import com.formacionbdi.springboot.app.usuarios.models.entity.Role;
+//
+//@RepositoryRestResource(path="roles")
+//public interface RoleDao extends PagingAndSortingRepository<Role, Long> {
+//
+//}
